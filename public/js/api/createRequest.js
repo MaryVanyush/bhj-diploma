3530bdcafe9ba081bed795fec3447a8f45a7b817
Maria Vanyushkina <m199178@mail.ru>
2023-04-11 17:23:45 +0300

    modified createRequest

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -4,21 +4,47 @@ const createRequest = (options) => {
     xhr.responseType = 'json';
     try {
         if(options.method === "GET"){
+            xhr.open(options.method, options.url);
             xhr.send();
             xhr.addEventListener('load', ( err, response ) => {
                     options.callback(err, response);
             });
         } else {              
-            const formData = new FormData()
-            xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            let formData = new FormData()  
+            
             for (const key in options.data) {
-                formData.append(key, options.data[key]);
+                formData[key] = options.data[key]
+                // formData.append(key, options.data[key])
                 console.log(formData)
             }
+            formData = JSON.stringify(formData)
+            console.log(formData)
+            xhr.setRequestHeader('Content-type', 'application/json');
+            xhr.onload = () => {
+                if(xhr.status === 200){
+                    console.log(xhr)
+                    const response = JSON.parse(xhr.responseText)
+                    options.callback(xhr.response.error, xhr.response.responseText )
+                } else {
+                    console.log(xhr.status)
+                    console.log(JSON.parse(xhr.responseText))
+                }
+            }
             xhr.send(formData);
-            options.callback(err, response);
             }
     } catch ( e ) {
         console.error("Произошла ошибка: ", e);
     }
 };
+
+createRequest({
+    url: '/user/login',
+    method: 'POST',
+    data: {
+        email: 'demo@demo',
+        password: 'demo'
+    },
+    callback: (e,r)=>{
+        console.log(e,r)
+    }
+})
